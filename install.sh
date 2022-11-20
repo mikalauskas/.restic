b348c6167b82fb6db009ea3ccb4502f242a40874
mikalauskas <rusm25@gmail.com>
2022-11-20 22:49:08 +0500

    initial

---
+++ install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/.env
+
+sudo chmod +x ${RESTIC_ROOT}/restic
+sudo chmod +x ${RESTIC_ROOT}/restic-backup.sh
+
+cat <<EOF > ${RESTIC_ROOT}/restic-backup.service
+[Unit]
+Description=Restic backup
+
+[Service]
+Type=simple
+ExecStart=${RESTIC_ROOT}/restic-backup.sh
+
+Nice=19
+IOSchedulingClass=2
+IOSchedulingPriority=7
+EOF
+
+sudo ln -s $HOME/.restic/restic-backup.service /etc/systemd/system/restic-backup.service
+sudo ln -s $HOME/.restic/restic-backup.timer /etc/systemd/system/restic-backup.timer
+
+sudo systemctl enable --now restic-backup.timer
+
+${RESTIC_ROOT}/restic init --repository-version latest
---
+++ restic-backup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+source ${SCRIPT_DIR}/.env
+
+backupExitCode=0
+date="date +%Y-%m-%dT%H:%M:%S%Z"
+
+function unlockRepo () {
+  echo "$($date): Unlock job: Begin"
+  echo "------------------------"
+  while [ "" != "$(restic -q list locks --no-lock --no-cache)" ]; do
+    echo "$($date): Unlock job: Unlocking"
+    echo "------------------------"
+    ${RESTIC_ROOT}/restic -q unlock --cleanup-cache
+    sleep 5
+  done
+  echo "$($date): Unlock job: End"
+  echo "------------------------"
+}
+
+function errorCheck () {
+  if [ $backupExitCode -eq 1 ]; then
+    echo "$($date): Unlock job: Begin. Checking repo."
+    echo "------------------------"
+    ${RESTIC_ROOT}/restic cache --cleanup --max-age 0
+    ${RESTIC_ROOT}/restic check --read-data-subset=1%
+    checkExitCode=$?
+    if [ $checkExitCode -eq 1 ]; then
+      echo "$($date): Unlock job: We found some errors. Rebuilding index."
+      echo "------------------------"
+      restic rebuild-index
+      rebuildExitCode=$?
+      if [ $rebuildExitCode -eq 1 ]; then
+        echo "$($date): Unlock job: Repo might have fatal errors. Rebuilding index and reading all packs."
+        echo "------------------------"
+        restic rebuild-index --read-all-packs
+        rebuildAllExitCode=$?
+        if [ $rebuildAllExitCode -eq 1 ]; then
+          echo "$($date): Unlock job: Fatal. Repo is dead."
+          echo "------------------------"
+          exit 0
+        fi
+      fi
+    fi
+  fi
+}
+
+function doBackup () {
+  echo "$($date): Backup job: Begin"
+  echo "------------------------"
+  ${RESTIC_ROOT}/restic cache --cleanup --max-age 0
+  ${RESTIC_ROOT}/restic backup -vv --compression max --host=${HOSTNAME} --exclude-file=${RESTIC_EXCLUDE_FILE} --files-from=${RESTIC_INCLUDE_FILE} --cleanup-cache
+  backupExitCode=$?
+  if [ ! $backupExitCode -eq 0 ]; then
+    errorCheck
+  fi
+  echo "$($date): Backup job: End"
+  echo "------------------------"
+  echo "$($date): Forget job: Begin"
+  echo "------------------------"
+  ${RESTIC_ROOT}/restic forget -vv --compression max --prune -d ${PRUNE_DAYS} -w ${PRUNE_WEEKS} -m ${PRUNE_MONTHS} --host=${HOSTNAME} --group-by host --cleanup-cache
+  echo "$($date): Forget job: End"
+  echo "------------------------"
+  return 0
+}
+
+function diff () {
+  echo "$($date): Diff job: Begin"
+  ${RESTIC_ROOT}/restic diff $(restic snapshots --compact | tail -n4 | head -n2 | cut -d' ' -f1)
+  echo "$($date): Diff job: End"
+  echo "------------------------"
+}
+
+dateStart=$(date '+%s')
+echo "------------------------"
+echo "------------------------"
+echo "------------------------"
+echo "$($date): Backup started"
+echo "------------------------"
+
+unlockRepo
+doBackup
+
+echo "$($date): Chown job: Begin"
+echo "------------------------"
+chown ${USERNAME}:${USERNAME} -R ${RESTIC_CACHE_DIR}
+echo "$($date): Chown job: End"
+echo "------------------------"
+
+# diff
+dateEnd=$(date '+%s')
+dateResult=$(date -d@$((dateEnd-dateStart)) -u '+%H:%M:%S')
+echo "$($date): Backup ended in $dateResult"
+echo "------------------------"
+echo "------------------------"
+echo "------------------------"
+exit $?
